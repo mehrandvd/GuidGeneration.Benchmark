847139b79598f4f91e3ee4442934c37ded7c0b71
Mehran Davoudi <mehran.davoudi@hotmail.com>
2022-12-10 03:46:18 +0330

    Add ToList to make it more real.

--- GuidGeneration.Benchmark/Program.cs
+++ GuidGeneration.Benchmark/Program.cs
@@ -10,33 +10,45 @@ public class GuidGenerationBenchmark
     private const int Size = 2_000_000;
 
     [Benchmark]
-    public void Loop_Simple()
+    public List<Guid> Loop_Simple()
     {
         var list = new List<Guid>();
         for (var i = 1; i <= Size; i++)
             list.Add(Guid.NewGuid());
-        _ = list.Count();
+        return list.ToList();
     }
 
     [Benchmark]
-    public void Loop_Fixed_Size()
+    public List<Guid> Loop_Fixed_Size()
     {
         var list = new List<Guid>(Size);
         for (var i = 0; i < Size; i++)
             list.Add(Guid.NewGuid());
+
+        return list;
     }
 
     [Benchmark]
-    public void Linq_Simple() 
+    public List<Guid> Linq_Simple() 
     {
-        _ = Enumerable
+        return Enumerable
             .Range(1, Size)
             .Select(n => Guid.NewGuid())
-            .Count();
+            .ToList();
     }
 
     [Benchmark]
-    public void Linq_Parallel()
+    public List<Guid> Linq_AsParallel()
+    {
+        return Enumerable
+            .Range(1, Size)
+            .AsParallel()
+            .Select(n => Guid.NewGuid())
+            .ToList();
+    }
+
+    [Benchmark]
+    public List<Guid> Linq_Parallel()
     {
         var list = new Guid[Size];
         Parallel.ForEach(list, (_, _, index) =>
@@ -44,17 +56,7 @@ public class GuidGenerationBenchmark
             list[index] = Guid.NewGuid();
         });
 
-        _ = list.Count();
-    }
-
-    [Benchmark]
-    public void Linq_AsParallel()
-    {
-        _ = Enumerable
-            .Range(1, Size)
-            .AsParallel()
-            .Select(n => Guid.NewGuid())
-            .Count();
+        return list.ToList();
     }
 }
 
